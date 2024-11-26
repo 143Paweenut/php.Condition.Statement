d06ecdd7adae84a4bef904ff139ee9b8a6c5747e
143Paweenut <66040233143@udru.ac.th>
2024-11-26 10:09:07 +0700

    primy

---
+++ globalvar.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Array </title>
+</head>
+<body>
+    <h1>Global Variable </h1>
+    <?php
+        $x = 75;
+        echo "ค่าของ x==>".$x;
+        echo "<br>";
+        function myfunction(){
+            //echo "อ่านจาก function ค่าของ x==>".$GLOBALS['x']; //เรียกใช้ x นอก fn
+            global $x;
+            $x =35;
+            echo "อ่านจาก function ค่าของ x==>".$x;
+        }
+
+        myfunction();
+        echo "<br>";
+        echo "ค่าของ function ค่าของ x==>".$x;
+
+        echo"<hr>";
+
+        echo "<h2>การใช้ข้อมูลจาก Server ด้วย \$_SERVER</h2>";
+        echo $_SERVER['PHP_SELF'];"<br>";
+        echo $_SERVER['SERVER_NAME'];"<br>";
+        //echo $_SERVER['HTTP_REFERER'];"<br>";
+        echo $_SERVER['HTTP_USER_AGENT'];"<br>";
+        echo $_SERVER['SA_NAME'];"<br>";
+        
+        echo"<hr>";
+    ?>
+</body>
+</html>
---
+++ phprequest.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>การใช้ PHP Request $_REQUEST </title>
+</head>
+<body>
+    <h1>โปรดกรอกชื่อท่าน </h1>
+    <form action ="<?php echo $_SERVER['PHP_SELF'];?>" method="post">
+        โปรดกรอกชื่อ : <input type="text" name="fname">
+        <input type="submit">
+        </form>
+        <?php
+        if($_SERVER['REQUEST_METHOD']=='POST'){
+        //$name = htmlspecialchars($_REQUEST[['fname']]);
+        $name = htmlspecialchars($_REQUEST[['fname']]);
+        if (empty($name)) {
+            echo "ชื่อเป็นที่ว่าง";
+        } else{
+            echo 'ยินดีต้อนรับคุณ'.$name;
+        }
+    }
+    ?>
+</body>
+</html>
